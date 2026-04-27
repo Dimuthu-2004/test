b5875f5e56e9a53970cb97a095fbb04682ca0ebd
Dimuthu Fonseka <kanchanafonseka2004@gmail.com>
2026-04-27 22:11:44 +0530

    edited

--- mern-item-manager/frontend/lab-test/src/api.js
+++ mern-item-manager/frontend/lab-test/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-baseURL: import.meta.env.VITE_API_URL || 'https://test-1pz8.vercel.app/api',
+baseURL: import.meta.env.VITE_API_URL || 'https://test-production-fa29.up.railway.app/api',
 });
 
 export const getItems = () => API.get('/items');
