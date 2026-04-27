139af43dc42ac2d833b7f31c7299a9ba2f08005a
Dimuthu Fonseka <kanchanafonseka2004@gmail.com>
2026-04-27 22:52:33 +0530

    fix: Remove trailing slash from CORS origin

--- mern-item-manager/backend/server.js
+++ mern-item-manager/backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: ['https://test-2jro.vercel.app/', 'http://localhost:3000'] }));
+app.use(cors({ origin: ['https://test-2jro.vercel.app', 'http://localhost:3000'] }));
 app.use(express.json());
 
 const itemRoutes = require('./routes/items');
